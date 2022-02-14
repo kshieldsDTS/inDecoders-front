fbd39daed4f2ca8115ff9a4045885bc8579b9f5a
kshieldsDTS <escanor05@gmail.com>
2022-02-14 14:05:36 -0500

    render API fetch data in UserCard.js

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -3,7 +3,11 @@ import React from 'react';
 function UserCard(props) {
     return (
         <div>
-            
+            <p>Name: {props.name}</p>
+            <p>Email: {props.email}</p>
+            <p>Skills: {props.skills}</p>
+            <p>Portfolio: {props.portfolio}</p>
+            <p>Payrate: ${props.payrate}/hour</p>
         </div>
     );
 }
