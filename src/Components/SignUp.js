b2ed38d8d29e214d827c77192af947302d62647b
kshieldsDTS <escanor05@gmail.com>
2022-02-14 17:07:52 -0500

    finish SignUp components and linked to backend, new users can now create accounts

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import api_url from '../apiConfig';
+import { Link, useNavigate } from 'react-router-dom'
 
 const initialFormData = {
 	username: '',
@@ -10,20 +12,31 @@ const initialFormData = {
 };
 
 function NewUser(props) {
+    const navigate = useNavigate();
     const [formData, setFormData] = useState(initialFormData)
     const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
     function handleChange(ev){
         setFormData({...formData, [ev.target.id]: ev.target.value})
     }
-    function handleSubmit(ev){
-        ev.preventDefault()
-        console.log(formData);
+    function handlePasswordMatch(ev) {
+			if (formData.password !== formData.re_password) {
+				setError(true);
+			} else {
+				setError(false);
+			}
+		};
+	function handleSubmit(ev){
+        ev.preventDefault();
+        sendData()
     }
-    function handlePasswordMatch(ev){
-        if (formData.password !== formData.re_password) {
-            setError(true);
-        } else {
-            setError(false);
+    const sendData = async() => {
+        const response = await axios.post(`${api_url}users/`, formData)
+        if (response.status === 201) {
+            setSuccess(true)
+            setTimeout(() => {
+                navigate('/login')
+            }, 5000)
         }
     }
     return (
@@ -35,10 +48,15 @@ function NewUser(props) {
 					<label>Email</label>
 					<input type='text' id='email' onChange={handleChange}></input>
 					<label>Password</label>
-					<input type='password' id='password' onChange={handleChange}></input>
+					<input type='password' id='password' onChange={handleChange} autoComplete='on'></input>
 					<label>Confirm Password</label>
-					<input type='password' id='re_password' onChange={handleChange}></input>
+					<input type='password' id='re_password' onChange={handleChange} onBlur={handlePasswordMatch} autoComplete='on'></input>
+                    {!error ? null : 'Passwords do not match!'}
                     <button type='submit' onClick={handleSubmit}>Sign Up!</button>
+                    {success ? 
+                        <p>
+                            You have successfully created an account with inDecoders. You will be automatically be redirected to log in. If you are not autimatically redirected, please click <Link to='/login'>here.</Link>
+                        </p> : null}
 				</form>
 			</div>
 		);
