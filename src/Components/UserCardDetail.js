e39600213fb0b5822d959d4b2b5a550ed459e195
kshieldsDTS <escanor05@gmail.com>
2022-02-16 18:34:54 -0500

    complete styling on UserCardDetail

--- src/Components/UserCardDetail.js
+++ src/Components/UserCardDetail.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import URL from '../apiConfig';
+import { CircularProgress } from '@mui/material';
 
 function UserCardDetail({ userInfo, loggedIn }) {
     const navigate = useNavigate();
@@ -18,21 +19,60 @@ function UserCardDetail({ userInfo, loggedIn }) {
             } catch (error) {}
         })();
     }, [id, navigate, userInfo.id]);
-	function test() {
-		console.log(userInfo.id, userData.id);
-	}
 	return (
-        <div>
-        {userData ? 
-        <div>
-			<p>Name: {userData.username}</p>
-            <p>Email: {userData.email}</p>
-            <p>Skills: {userData.skills}</p>
-            <p>About: {userData.bio}</p>
-            <p>Portfolio: {userData.portfolio}</p>
-            <p>Pay: ${userData.payrate}/hour</p>
-			<button onClick={test}>Test the Thing</button>
-		</div> : <p>Loading... please wait...</p>}
+		<div className='detail-wrapper'>
+			<div className='user-card-wrapper'>
+				{userData ? (
+					<div className='user-card'>
+						<div className='user-info'>
+							<div className='item'>
+								<p className='label'>Username:</p>
+								<p className='value'>{userData.username}</p>
+							</div>
+							<div className='item'>
+								<p className='label'>Email:</p>
+								<p className='value'>{userData.email}</p>
+							</div>
+						</div>
+						<div className='user-detail'>
+							<div className='item'>
+								<p className='label'>Skills:</p>
+								<p className='value'>{userData.skills}</p>
+							</div>
+							<div className='item'>
+								<p className='label'>About:</p>
+								<p className='value'>{userData.bio}</p>
+							</div>
+							<div className='item'>
+								<p className='label'>Portfolio:</p>
+								<p className='value'>{userData.portfolio}</p>
+							</div>
+							<div className='item'>
+								<p className='label'>Availability</p>
+								<div className='days value'>
+									<p className={userData.sunday ? 'green' : 'gray'}>Sun</p>
+									<p className={userData.monday ? 'green' : 'gray'}>Mon</p>
+									<p className={userData.tuesday ? 'green' : 'gray'}>Tue</p>
+									<p className={userData.wednesday ? 'green' : 'gray'}>Wed</p>
+									<p className={userData.thursday ? 'green' : 'gray'}>Thu</p>
+									<p className={userData.friday ? 'green' : 'gray'}>Fri</p>
+									<p className={userData.saturday ? 'green' : 'gray'}>Sat</p>
+								</div>
+							</div>
+                            <div className='item'>
+                                <p className='label'>
+                                    Payrate:
+                                </p>
+                                <p className='value'>
+                                    ${userData.payrate} per hour
+                                </p>
+                            </div>
+						</div>
+					</div>
+				) : (
+					<CircularProgress color='secondary' />
+				)}
+			</div>
 		</div>
 	);
 }
